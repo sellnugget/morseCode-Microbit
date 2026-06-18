c2a508261299d4d8e59538eb213328ccf177360d
sellnugget <96460405+sellnugget@users.noreply.github.com>
2026-06-18 16:54:08 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,30 +12,11 @@ enum Signal{
 namespace MorseCode {
 
 
+    //% block="send morse $signals"
+    //% signals.shadow="lists_create_with"
+    export function sendMorse(signals: Signal[]) {
+        for (let s of signals) {
 
-    //% block="Letter $a"
-    export function PlayLetter1(a: Signal)
-    {
-
-    }
-    //% block="Letter $a $b"
-    export function PlayLetter2(a: Signal, b: Signal)
-    {
-
-    }
-     //% block="Letter $a $b $c"
-    export function PlayLetter3(a: Signal, b: Signal, c: Signal)
-    {
-
-    }
-     //% block="Letter $a $b $c $d"
-    export function PlayLetter4(a: Signal, b: Signal, c: Signal, d: Signal)
-    {
-
-    }
-     //% block="Letter $a $b $c $d $e"
-    export function PlayLetter5(a: Signal, b: Signal, c: Signal, d: Signal, e: Signal )
-    {
-
+        }
     }
 }
